90e24f639c88c7e3caa98c22703f9e7cbfa39bee
tranelb <tranelb@users.noreply.github.com>
2016-09-27 10:55:57 -0700

    Add files via upload

---
+++ shutil_drill_v002.py
@@ -0,0 +1,13 @@
+#Tech Academy python drill
+#item 57 of 63
+import shutil
+
+
+shutil.move('C:/Users/Tranel/Desktop/Folder_A/randomFile_01.txt',
+            'C:/Users/Tranel/Desktop/Folder_B')
+shutil.move('C:/Users/Tranel/Desktop/Folder_A/randomFile_02.txt',
+            'C:/Users/Tranel/Desktop/Folder_B')
+shutil.move('C:/Users/Tranel/Desktop/Folder_A/randomFile_03.txt',
+            'C:/Users/Tranel/Desktop/Folder_B')
+shutil.move('C:/Users/Tranel/Desktop/Folder_A/randomFile_04.txt',
+            'C:/Users/Tranel/Desktop/Folder_B')
