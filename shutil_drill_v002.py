d270ae1b2b97d3792c4ea5f7fb3ebb8c51742aad
tranelb <tranelb@users.noreply.github.com>
2016-09-28 10:05:44 -0700

    removed hard coded links

--- shutil_drill_v002.py
+++ shutil_drill_v002.py
@@ -1,13 +1,26 @@
 #Tech Academy python drill
 #item 57 of 63
 import shutil
+import os
+
+#shutil.move('C:/Users/Tranel/Desktop/Folder_A/randomFile_01.txt',
+#            'C:/Users/Tranel/Desktop/Folder_B')
+#shutil.move('C:/Users/Tranel/Desktop/Folder_A/randomFile_02.txt',
+#            'C:/Users/Tranel/Desktop/Folder_B')
+#shutil.move('C:/Users/Tranel/Desktop/Folder_A/randomFile_03.txt',
+#            'C:/Users/Tranel/Desktop/Folder_B')
+#shutil.move('C:/Users/Tranel/Desktop/Folder_A/randomFile_04.txt',
+#            'C:/Users/Tranel/Desktop/Folder_B')
+            
+            
+strtPath = raw_input('Enter start path\\folder: ')
+
+dirs = os.listdir(strtPath)
+
+dstPath = raw_input('Enter destination path\\folder: ')
 
 
-shutil.move('C:/Users/Tranel/Desktop/Folder_A/randomFile_01.txt',
-            'C:/Users/Tranel/Desktop/Folder_B')
-shutil.move('C:/Users/Tranel/Desktop/Folder_A/randomFile_02.txt',
-            'C:/Users/Tranel/Desktop/Folder_B')
-shutil.move('C:/Users/Tranel/Desktop/Folder_A/randomFile_03.txt',
-            'C:/Users/Tranel/Desktop/Folder_B')
-shutil.move('C:/Users/Tranel/Desktop/Folder_A/randomFile_04.txt',
-            'C:/Users/Tranel/Desktop/Folder_B')
+
+for var in dirs:
+    varN = strtPath + '\\' + var
+    shutil.move(varN, dstPath)
